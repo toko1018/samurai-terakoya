f653bd3ba9b616ddd08e9c07c6239affc4a9e6e9
toko1018 <toko.sa10mi@gmail.com>
2025-01-20 14:11:35 +0900

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,10 @@
+// head要素を取得し、中身を出力する
+console.log(document.head);
+// body要素を取得し、中身を出力する
+console.log(document.body);
+
+// windowオブジェクトの中身を出力する
+console.log(window);
+
+// HTML要素をidで取得し、中身を出力する
+console.log(document.getElementById('first-list'));
